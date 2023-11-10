ea974288ca6fb727e124720024f5373f505c05d4
pathikrit <pathikritbhowmick@msn.com>
2023-11-10 10:21:02 -0500

    rm config.json

--- README.md
+++ README.md
@@ -6,9 +6,9 @@ Random collection of my chrome extensions I find useful for daily browsing
 
 1. Checkout this repo:
 ```shell
-git clone chrome_ai
+git clone git@github.com:pathikrit/chrome_ai.git
 ```
 
-2. Set your [`OPENAI_API_KEY`](https://platform.openai.com/api-keys) in [config.json](config.json)
+2. Set your [`OPENAI_API_KEY`](https://platform.openai.com/api-keys) in [chatgpt.js](chatgpt.js)
 
 3. Load this as a [Chrome extension](https://developer.chrome.com/docs/extensions/mv3/getstarted/development-basics/#load-unpacked)
--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-let config = undefined
-
 const tools = [
   {
     id: 'gcal',
     title: 'To Google calendar',
     runInTab: () => document.body.innerText,
-    fn: (tab, text) => textToCal(tab.url, text)
+    fn: (tab, text) => {
+      log('Calling ChatGPT ...')
+      return chatGptFunctionCall(
+        `I saved the text from a webpage (url=${tab.url}). I will paste it below. Can you create a function call out of it?` + text,
+        {
+          name: 'create_calendar_invite',
+          description: 'Creates a calendar invite with given title, start date and time, end date and time, location and description',
+          parameters: {
+            type: "object",
+            properties: {
+              title: {type: "string", description: "Event title"},
+              start: {type: "string", format: "date-time", description: "Event start time in ISO format"},
+              end: {type: "string", format: "date-time", description: "Event end time in ISO format"},
+              location: {type: "string", description: "Event location"},
+              details: {type: "string", description: "Event description (short)"}
+            },
+            required: ["title", "start", "end"],
+          }
+        },
+        (arg) => {
+          const dateFormat = (d) => d.replaceAll('-', '').replaceAll(':', '')
+          const link = `https://calendar.google.com/calendar/render?action=TEMPLATE&text=${arg.title}&dates=${dateFormat(arg.start)}/${dateFormat(arg.end)}&location=${arg.location ?? ''}&details=${arg.details ?? ''}`
+          window.open(encodeURI(link), '_blank')
+        },
+        (res) => log(JSON.stringify(res))
+      )
+    }
   },
   {
     id: 'outlook',
@@ -25,8 +49,7 @@ const tools = [
 ]
 
 $(document).ready(async () => {
-  config = await fetch('config.json').then(res => res.json())
-  const tab = await currentTab()
+  const tab = await chrome.tabs.query({active: true, lastFocusedWindow: true}).then(([tab]) => tab)
   if (tab) {
     for (const tool of tools) {
       if (tab.url.includes(tool.urlFilter ?? '')) {
@@ -36,35 +59,5 @@ $(document).ready(async () => {
     }
   }
 })
-
-currentTab = () => chrome.tabs.query({active: true, lastFocusedWindow: true}).then(([tab]) => tab)
-
-textToCal = (url, text) => {
-  log('Calling ChatGPT ...')
-  return chatGptFunctionCall(
-    `I saved the text from a webpage (url=${url}). I will paste it below. Can you create a function call out of it?` + text,
-    {
-      name: 'create_calendar_invite',
-      description: 'Creates a calendar invite with given title, start date and time, end date and time, location and description',
-      parameters: {
-        type: "object",
-        properties: {
-          title: {type: "string", description: "Event title"},
-          start: {type: "string", format: "date-time", description: "Event start time in ISO format"},
-          end: {type: "string", format: "date-time", description: "Event end time in ISO format"},
-          location: {type: "string", description: "Event location"},
-          details: {type: "string", description: "Event description (short)"}
-        },
-        required: ["title", "start", "end"],
-      }
-    },
-    (arg) => {
-      const dateFormat = (d) => d.replaceAll('-', '').replaceAll(':', '')
-      const link = `https://calendar.google.com/calendar/render?action=TEMPLATE&text=${arg.title}&dates=${dateFormat(arg.start)}/${dateFormat(arg.end)}&location=${arg.location ?? ''}&details=${arg.details ?? ''}`
-      window.open(encodeURI(link), '_blank')
-    },
-    (res) => log(JSON.stringify(res))
-  )
-}
 
 const log = (text) => $('#logs').append('\n' + text)
