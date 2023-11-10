b951fbfa8ff59732906962913660bc9170956f15
pathikrit <pathikritbhowmick@msn.com>
2023-11-10 10:15:00 -0500

    move chatgpt to own file

---
+++ chatgpt.js
@@ -0,0 +1,21 @@
+OPENAI_API_KEY = 'XX-XXXXXXXXXXXXXXXXXXXXXXXXXX'
+
+const chatGptFunctionCall = (
+  prompt,
+  functionSchema,
+  fn,
+  noFn,
+  systemMsg = `If needed, you can assume today's date is: ${new Date().toLocaleDateString()}`,
+  model = 'gpt-3.5-turbo'
+) => $.post({
+    url: 'https://api.openai.com/v1/chat/completions',
+    headers: {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + OPENAI_API_KEY},
+    data: JSON.stringify({
+      model: model,
+      messages: [{role: 'system', content: systemMsg}, {role: 'user', content: prompt}],
+      tools: [{type: 'function', function: functionSchema}]
+    })
+  }).then(res => {
+    const { arguments } = res?.choices?.[0]?.message?.tool_calls?.[0]?.function
+    return arguments ? fn(JSON.parse(arguments)) : noFn(res)
+  })
--- index.js
+++ index.js
@@ -41,56 +41,30 @@ currentTab = () => chrome.tabs.query({active: true, lastFocusedWindow: true}).th
 
 textToCal = (url, text) => {
   log('Calling ChatGPT ...')
-  return $.post({
-    url: 'https://api.openai.com/v1/chat/completions',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer ' + config.OPENAI_API_KEY
+  return chatGptFunctionCall(
+    `I saved the text from a webpage (url=${url}). I will paste it below. Can you create a function call out of it?` + text,
+    {
+      name: 'create_calendar_invite',
+      description: 'Creates a calendar invite with given title, start date and time, end date and time, location and description',
+      parameters: {
+        type: "object",
+        properties: {
+          title: {type: "string", description: "Event title"},
+          start: {type: "string", format: "date-time", description: "Event start time in ISO format"},
+          end: {type: "string", format: "date-time", description: "Event end time in ISO format"},
+          location: {type: "string", description: "Event location"},
+          details: {type: "string", description: "Event description (short)"}
+        },
+        required: ["title", "start", "end"],
+      }
     },
-    data: JSON.stringify({
-      model: 'gpt-3.5-turbo',
-      messages: [
-        {role: 'system', content: `If needed, you can assume today's date is: ${new Date().toLocaleDateString()}`},
-        {role: 'user', content: `I saved the text from a webpage (url=${url}). I will paste it below. Can you create a function call out of it?` + text
-        }
-      ],
-      tools: [
-        {
-          type: 'function',
-          function: {
-            name: 'create_calendar_invite',
-            description: 'Creates a calendar invite with given title, start date and time, end date and time, location and description',
-            parameters: {
-              type: "object",
-              properties: {
-                title: {type: "string", description: "Event title"},
-                start: {type: "string", format: "date-time", description: "Event start time in ISO format"},
-                end: {type: "string", format: "date-time", description: "Event end time in ISO format"},
-                location: {type: "string", description: "Event location"},
-                details: {type: "string", description: "Event description (short)"}
-              },
-              required: ["title", "start", "end"],
-            }
-          }
-        }
-      ]
-    })
-  }).then(res => {
-    const fn = res?.choices?.[0]?.message?.tool_calls?.[0]?.function
-    if (fn) {
-      const link = gCalLink(JSON.parse(fn.arguments))
-      log(link)
-      window.open(link, '_blank')
-    } else {
-      log(JSON.stringify(res))
-    }
-  })
+    (arg) => {
+      const dateFormat = (d) => d.replaceAll('-', '').replaceAll(':', '')
+      const link = `https://calendar.google.com/calendar/render?action=TEMPLATE&text=${arg.title}&dates=${dateFormat(arg.start)}/${dateFormat(arg.end)}&location=${arg.location ?? ''}&details=${arg.details ?? ''}`
+      window.open(encodeURI(link), '_blank')
+    },
+    (res) => log(JSON.stringify(res))
+  )
 }
 
-gCalLink = (arg) => {
-  const dateFormat = (d) => d.replaceAll('-', '').replaceAll(':', '')
-  const link = `https://calendar.google.com/calendar/render?action=TEMPLATE&text=${arg.title}&dates=${dateFormat(arg.start)}/${dateFormat(arg.end)}&location=${arg.location ?? ''}&details=${arg.details ?? ''}`
-  return encodeURI(link)
-}
-
-log = (text) => $('#logs').append('\n' + text)
+const log = (text) => $('#logs').append('\n' + text)
