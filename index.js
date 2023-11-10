b72cbd354f42f33ed683e0ba43090af0fa834fab
pathikrit <pathikritbhowmick@msn.com>
2023-11-09 22:07:23 -0500

    poc of gcal link construction

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 $('#gcal').on('click', () => {
   //tabToText().then(log)
-  askChatGPT("Let's meet tomorrow at Bernie's at 2pm").then(res => log(JSON.stringify(res)))
+  textToCal("Let's meet tomorrow at Bernie's at 2pm")
 });
 
 tabToText = () => chrome.tabs.query({active: true, lastFocusedWindow: true})
@@ -9,7 +9,8 @@ tabToText = () => chrome.tabs.query({active: true, lastFocusedWindow: true})
 
 OPENAI_API_KEY=
 
-askChatGPT = (text) => {
+textToCal = (text) => {
+  log('Calling ChatGPT ...')
   return $.post({
     url: 'https://api.openai.com/v1/chat/completions',
     headers: {
@@ -47,18 +48,39 @@ askChatGPT = (text) => {
                   format: "date-time",
                   description: "Event end time"
                 },
+                location: {
+                  type: "string",
+                  description: "Event location"
+                },
                 details: {
                   type: "string",
-                  description: "Event description"
+                  description: "Event description (short)"
                 }
               },
-              required: ["title", "start"],
+              required: ["title", "start", "end"],
             }
           }
         }
       ]
     })
+  }).then(res => {
+    log('Received msg', res)
+    const fn = res?.choices?.[0]?.message?.tool_calls?.[0]?.function
+    if (fn) {
+      const link = gCalLink(JSON.parse(fn.arguments))
+      log(link)
+    } else {
+      log(res)
+    }
   })
+}
+
+// https://calendar.google.com/calendar/render?action=TEMPLATE&text=Meeting%20at%20Bernie's&dates=20211103T140000Z/20211103T150000Z&location=Bernie's&details=
+
+gCalLink = (arg) => {
+  const dateFormat = (d) => d.replaceAll('-', '').replaceAll(':', '')
+  const link = `https://calendar.google.com/calendar/render?action=TEMPLATE&text=${arg.title}&dates=${dateFormat(arg.start)}/${dateFormat(arg.end)}&location=${arg.location ?? ''}&details=${arg.details ?? ''}`
+  return encodeURI(link)
 }
 
 log = (text) => {
