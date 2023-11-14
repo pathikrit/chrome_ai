3d6ce62129c75c42df1ce7a7cfdc68b4c44b3977
pathikrit <pathikritbhowmick@msn.com>
2023-11-14 10:37:52 -0800

    find in gmail

--- content-script.js
+++ content-script.js
@@ -6,21 +6,23 @@ const fns = {
       .filter(el => el.innerText === 'Amazon')
       .map(el => el.nextElementSibling.nextElementSibling)
       .forEach(el => {
-        const url = `https://www.amazon.com/gp/your-account/order-history?${amazon_amount_search_key}=${el.innerText}`
-        el.nextElementSibling.nextElementSibling.insertAdjacentHTML('afterend', `<a href="${url}" target="_blank">Find in Amazon</a>`)
+        const amount = el.innerText.replace('-', '').replace('$', '')
+        const amazonUrl = `https://www.amazon.com/gp/your-account/order-history?${amazon_amount_search_key}=${amount}`
+        el.insertAdjacentHTML('afterend', `<br/><a href="${amazonUrl}" target="_blank">Find in Amazon</a>`)
+
+        const gmailUrl = `https://mail.google.com/mail/u/0/#search/amazon+${amount}`
+        el.insertAdjacentHTML('afterend', `<a href="${gmailUrl}" target="_blank">Find in GMail</a>`)
       })
   },
   amazon_search:  () => {
-    const amount = new URLSearchParams(window.location.search).get(amazon_amount_search_key).replace('-$', '')
-    console.log(amount)
+    const amount = new URLSearchParams(window.location.search).get(amazon_amount_search_key)
+    window.find(amount)
   }
 }
 
 const url = window.location.href
-let fn = undefined
 if (url.includes('mint.intuit.com')) {
-  fn = fns.mint
+  setTimeout(fns.mint, 5000)
 } else if (url.includes(amazon_amount_search_key)) {
-  fn = fns.amazon_search
+  setTimeout(fns.amazon_search, 500)
 }
-setTimeout(fn, 5000)
