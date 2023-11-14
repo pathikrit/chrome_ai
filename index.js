bdc9501c104d37d41bc56798ee2f35125f7313f9
pathikrit <pathikritbhowmick@msn.com>
2023-11-14 08:49:57 -0800

    localize to scope popup/modal

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const selectionOrText = () => {
 const tools = [
   {
     id: 'chat',
-    title: 'Chat with Page',
+    title: 'Chat with page',
     detail: 'Opens ChatGPT (with prompt in clipboard) to chat with page',
     runInTab: selectionOrText,
     fn: (tab, page) => {
@@ -21,12 +21,12 @@ const tools = [
         query
       ].join('\n\n')
       log(`Opening ChatGPT with ${page.length} chars page ...`)
-      copy(prompt).then(() => openUrl('https://chat.openai.com/'))
+      copy(prompt).then(() => open('https://chat.openai.com/'))
     }
   },
   {
     id: 'gcal',
-    title: 'To Google calendar',
+    title: 'To Google Calendar',
     detail: 'Create Google calendar invite from contents of this page',
     runInTab: selectionOrText,
     fn: (tab, text) => askChatGpt(
@@ -36,7 +36,7 @@ const tools = [
         f: (arg) => {
           const dateFormat = (d) => d.replaceAll('-', '').replaceAll(':', '').replaceAll('Z', '')
           const link = `https://calendar.google.com/calendar/render?action=TEMPLATE&text=${arg.title}&dates=${dateFormat(arg.start)}/${dateFormat(arg.end)}&location=${arg.location ?? ''}&details=${arg.details ?? ''}`
-          openUrl(encodeURI(link))
+          open(encodeURI(link))
         },
         schema: {
           name: 'create_calendar_invite',
@@ -64,7 +64,7 @@ const tools = [
     runInTab: () => Array.from(document.querySelectorAll('div[aria-selected="true"] span[title*="@"]')).map(el => el.title),
     fn: (tab, emails) => {
       if (emails && emails.length > 0) {
-        copy(emails.join('; ')).then(() => openUrl('https://outlook.live.com/mail/0/options/mail/rules'))
+        copy(emails.join('; ')).then(() => open('https://outlook.live.com/mail/0/options/mail/rules'))
       } else {
         log('No email selected')
       }
@@ -102,43 +102,57 @@ const tools = [
     },
     fn: (tab, links) => {
       log(links.join('\n'))
-      links.forEach(link => openUrl(link))
+      links.forEach(link => open(link))
+    }
+  },
+  {
+    id: 'amzn',
+    title: `Find Amazon Txn`,
+    detail: `Find Amazon Txn`,
+    urlFilter: 'amazon.com/gp/your-account/order-history',
+    runInTab: () => {
+      document.body.focus()
+      return window.find('$156')
+    },
+    fn: (tab, res) => {
+      log(res)
     }
   },
 ]
 
 $(document).ready(async () => {
-  $('input').change(() => $('#save').prop('disabled', false).text('Save'))
-
-  $('#save').click(() => {
-    settings = {}
-    $('input').toArray().forEach((el) => {settings[el.id] = el.value})
-    chrome.storage.sync.set(settings).then(() => $('#save').prop('disabled', true).text(`Saved`))
-  })
-
   settings = await chrome.storage.sync.get(null)
-  Object.entries(settings).forEach(([key, value]) => $('#' + key).val(value))
-
-  const tab = await chrome.tabs.query({active: true, lastFocusedWindow: true}).then(([tab]) => tab)
-  if (tab) {
-    tools.forEach(tool => {
-      if (tab.url.includes(tool.urlFilter ?? '')) {
-        const click = () => chrome.scripting
-          .executeScript({target: {tabId: tab.id}, function: tool.runInTab})
-          .then(([{result}]) => tool.fn(tab, result))
-        $('<button>', {id: tool.id, title: tool.detail, 'data-tooltip': tool.detail})
-          .text(tool.title)
-          .toggleClass('outline', tool.urlFilter == null)
-          .click(click)
-          .appendTo($('#tools'))
-      }
-    })
-  }
 
   if (new URL(document.location).searchParams.get('mode') === 'options' || !settings.openai_api_key) {
+    $('input').change(() => $('#save').prop('disabled', false).text('Save'))
+
+    $('#save').click(() => {
+      settings = {}
+      $('input').toArray().forEach((el) => {settings[el.id] = el.value})
+      chrome.storage.sync.set(settings).then(() => $('#save').prop('disabled', true).text(`Saved`))
+    })
+
+    Object.entries(settings).forEach(([key, value]) => $('#' + key).val(value))
+
     $('#main').hide()
     $('#options').show()
   } else {
+    const tab = await chrome.tabs.query({active: true, lastFocusedWindow: true}).then(([tab]) => tab)
+    if (tab) {
+      tools.forEach(tool => {
+        if (tab.url.includes(tool.urlFilter ?? '')) {
+          const click = () => chrome.scripting
+            .executeScript({target: {tabId: tab.id}, function: tool.runInTab})
+            .then(([{result}]) => tool.fn(tab, result))
+          $('<button>', {id: tool.id, title: tool.detail, 'data-tooltip': tool.detail})
+            .text(tool.title)
+            .toggleClass('outline', tool.urlFilter == null)
+            .click(click)
+            .appendTo($('#tools'))
+        }
+      })
+    }
+
     $('#main').show()
     $('#options').hide()
   }
@@ -181,6 +195,6 @@ const askChatGpt = (
 
 const copy = (text) => navigator.clipboard.writeText(text)
 
-const openUrl = (url, tab) => { tab ? chrome.tabs.update(tab.id, {url}) : chrome.tabs.create({url}) }
+const open = (url, tab) => { tab ? chrome.tabs.update(tab.id, {url}) : chrome.tabs.create({url}) }
 
 const log = (text) => $('#logs').text(text)
