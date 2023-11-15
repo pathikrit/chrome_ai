4db2281edb397608f3f38607ccb09a7fd1eb11fe
pathikrit <pathikritbhowmick@msn.com>
2023-11-15 13:52:52 -0800

    frameworkify page script

--- content-script.js
+++ content-script.js
@@ -1,31 +1,34 @@
-const amazon_amount_search_key = '__chrome_ai_amount'
+const constants = {
+  amazon_amount_search_key: '__chrome_ai_amount'
+}
 
-const fns = {
-  mint: () => {
-    Array.from(document.querySelectorAll('td[role="cell"]'))
-      .filter(el => el.innerText === 'Amazon')
-      .map(el => el.nextElementSibling.nextElementSibling)
-      .forEach(el => {
-        const insertUrl = (name, url) => el.insertAdjacentHTML('afterend', `<a href="${url}" target="_blank">Find in ${name}</a><br/>`)
-        const amount = el.innerText.replace('-', '').replace('$', '')
-        insertUrl('Amazon', `https://www.amazon.com/gp/your-account/order-history?${amazon_amount_search_key}=${amount}`)
-        insertUrl('GMail', `https://mail.google.com/mail/u/0/#search/amazon+${amount}`)
-      })
+const pageScripts = [
+  {
+    urlFilter: 'mint.intuit.com',
+    delay: 5000,
+    fn: () => {
+      Array.from(document.querySelectorAll('td[role="cell"]'))
+        .filter(el => el.innerText === 'Amazon')
+        .map(el => el.nextElementSibling.nextElementSibling)
+        .forEach(el => {
+          const insertUrl = (name, url) => el.insertAdjacentHTML('afterend', `<a href="${url}" target="_blank">Find in ${name}</a><br/>`)
+          const amount = el.innerText.replace('-', '').replace('$', '')
+          insertUrl('Amazon', `https://www.amazon.com/gp/your-account/order-history?${constants.amazon_amount_search_key}=${amount}`)
+          insertUrl('GMail', `https://mail.google.com/mail/u/0/#search/amazon+${amount}`)
+        })
+    }
   },
-  amazon_search:  () => {
-    const amount = new URLSearchParams(window.location.search).get(amazon_amount_search_key)
-    window.find(amount)
+  {
+    urlFilter: constants.amazon_amount_search_key,
+    fn: () => {
+      const amount = new URLSearchParams(window.location.search).get(constants.amazon_amount_search_key)
+      window.find(amount)
+    }
   }
-}
+]
 
-const main = () => {
-  const url = window.location.href
-  if (url.includes('mint.intuit.com')) {
-    setTimeout(fns.mint, 5000)
-  } else if (url.includes(amazon_amount_search_key)) {
-    fns.amazon_search()
-  }
-}
+const main = () => pageScripts
+  .filter(script => window.location.href.includes(script.urlFilter))
+  .forEach(script => setTimeout(script.fn, script.delay ?? 1))
 
 main()
-window.addEventListener('hashchange', main)
